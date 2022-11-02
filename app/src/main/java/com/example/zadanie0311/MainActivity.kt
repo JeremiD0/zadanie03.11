2e207c2e5b688e7e5ec0d1e7c642ac7d9723f736
JeremiD0 <damski.jeremi00@gmail.com>
2022-11-02 20:22:42 +0100

    Last

--- app/src/main/java/com/example/zadanie0311/MainActivity.kt
+++ app/src/main/java/com/example/zadanie0311/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import org.w3c.dom.Text
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         val FirstEl = findViewById<TextView>(R.id.FirstEl)
         val stosCount = findViewById<TextView>(R.id.stosCount)
 
-        // Dodawanie
+        // DODAWANIE DO STOSU
         findViewById<Button>(R.id.dodajBtn).setOnClickListener {
             if (stosinput.toString() != ""){
                 liczba = stosinput.text.toString().toInt()
@@ -30,7 +31,7 @@ class MainActivity : AppCompatActivity() {
                 stosCount.text = "Stos \n wpisz element"
             }
         }
-        // Usuwanie
+        // USUWANIE ZE STOSU
         findViewById<Button>(R.id.usunBtn).setOnClickListener {
             if (!stos.isEmpty()){
                 val liczba = stosinput.text.toString().toInt()
@@ -42,7 +43,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        // Ostatni Element
+        // OSTATNI ELEMENT STOSU
         findViewById<Button>(R.id.LastElement).setOnClickListener {
             if (!stos.isEmpty()){
                 liczba = stos.last()
@@ -53,6 +54,57 @@ class MainActivity : AppCompatActivity() {
                 FirstEl.text = "stos jest pusty!"
             }
         }
+
+
+        val kolejka: Queue<Int> = LinkedList<Int>()
+        var liczba1 = 1
+        var elementy = 0
+
+        val KolejkaCOunt = findViewById<TextView>(R.id.kolejkaCount)
+
+        // DODAWANIE DO KOLEJKI
+        findViewById<Button>(R.id.dodajKBtn).setOnClickListener {
+            kolejka.add(liczba1)
+            liczba1 += 1
+            elementy +=1
+            KolejkaCOunt.text = "Kolejka \n " + elementy.toString()
+        }
+
+        // USUWANIE Z KOLEJKI
+        findViewById<Button>(R.id.usunKBtn).setOnClickListener {
+            if (!kolejka.isEmpty()){
+                kolejka.remove()
+                elementy -= 1
+                KolejkaCOunt.text = "Kolejka \n " + elementy.toString()
+            }
+            else{
+                KolejkaCOunt.text = "Kolejka \n KOLEJKA JEST PUSTA"
+            }
+        }
+
+        //PIERWSZY ELEMENT KOLEJKI
+        findViewById<Button>(R.id.FirstKol).setOnClickListener {
+            if (!kolejka.isEmpty()){
+                val first = kolejka.element()
+                findViewById<TextView>(R.id.firstEl).text = first.toString()
+            }
+            else{
+                findViewById<TextView>(R.id.firstEl).text = "KOLEJKA JEST PUSTA"
+            }
+        }
+
+        // OSTATNI ELEMENT KOLEJKI
+        findViewById<Button>(R.id.ostElement).setOnClickListener {
+            if (!kolejka.isEmpty()){
+                val last = kolejka.last()
+                findViewById<TextView>(R.id.LasttEl2).text = last.toString()
+            }
+            else{
+                findViewById<TextView>(R.id.LasttEl2).text = "KOLEJKA JEST PUSTA"
+            }
+        }
+
+
     }
 
 
